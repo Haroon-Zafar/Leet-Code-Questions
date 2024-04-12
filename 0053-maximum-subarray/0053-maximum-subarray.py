a6d1c86a5164b284e9fc386688b011ecf7c2c9f8
Muhammad Haroon Zafar <62334575+Haroon-Zafar@users.noreply.github.com>
2024-04-12 16:23:46 +0500

    Time: 571 ms (13.88%), Space: 31 MB (38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,26 +1,36 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # using kadane' algorithm
-        current_sum = nums[0]
-        maximum_sum = nums[0]
+        # current_sum = nums[0]
+        # maximum_sum = nums[0]
 
-        # If there is only a single element in the list
-        if len(nums) == 1 :
-            return(nums[0])
+        # # If there is only a single element in the list
+        # if len(nums) == 1 :
+        #     return(nums[0])
         
-        for index in range(1, len(nums)):
+        # for index in range(1, len(nums)):
 
-            # if sum is negative
-            if current_sum < 0 :
-                current_sum = nums[index]
+        #     # if sum is negative
+        #     if current_sum < 0 :
+        #         current_sum = nums[index]
             
-            # if sum is positive 
-            else : 
-                current_sum += nums[index]
+        #     # if sum is positive 
+        #     else : 
+        #         current_sum += nums[index]
             
-            if current_sum > maximum_sum : 
-                # update the maximum_sum 
-                maximum_sum = current_sum
-        return (maximum_sum)
+        #     if current_sum > maximum_sum : 
+        #         # update the maximum_sum 
+        #         maximum_sum = current_sum
+        # return (maximum_sum)
 
-        
+        max_current = max_global = nums[0]  # Initialize both variables with the first element of the array
+
+        for num in nums[1:]:
+            # Update max_current to be the maximum of current element and the sum so far
+            max_current = max(num, max_current + num)
+            
+            # Update max_global if max_current is greater
+            max_global = max(max_global, max_current)
+
+        return max_global
+            
