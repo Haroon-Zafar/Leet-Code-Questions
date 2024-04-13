11f651ad8bf84aae703e00a4791ff440bc87dc93
Muhammad Haroon Zafar <62334575+Haroon-Zafar@users.noreply.github.com>
2024-04-13 20:44:11 +0500

    Time: 30 ms (91.15%), Space: 16.6 MB (34.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,26 +1,12 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':
-            return 0
-        
-        len_of_str = len(s)
-        prev_count = 1
-        prev_prev_count = 0
-        
-        for index in range(len_of_str):
-            current_count = 0
-            
-            # Single-digit decoding
-            if s[index] != '0':
-                current_count += prev_count
-            
-            # Two-digit decoding
-            if index > 0 and (s[index - 1] == '1' or (s[index - 1] == '2' and s[index] <= '6')):
-                current_count += prev_prev_count
-            
-            # Update counts for the next iteration
-            prev_prev_count = prev_count
-            prev_count = current_count
-            
-        return prev_count
-                    
+        count, single_digit, double_digit = 0, 1, 0
+        for i in range(len(s) -1, -1, -1):
+            if s[i] != "0":
+                count += single_digit
+            if (s[i] == "1") or ((i + 1 < len(s)) and (s[i] == "2") and (int(s[i+1]) <= 6 )):
+                count += double_digit
+            double_digit = single_digit
+            single_digit = count
+            count = 0
+        return single_digit
