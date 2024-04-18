da2148a661771e8a9512a211aa0115a4d8efa37d
Muhammad Haroon Zafar <62334575+Haroon-Zafar@users.noreply.github.com>
2024-04-18 23:50:44 +0500

    Time: 48 ms (9.19%), Space: 16.9 MB (34.43%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -14,16 +14,26 @@ class Solution:
         
         visited = {}
         
+        print("Visited", visited)
+        print("before dfs")
+
         def dfs(original):
             if original in visited:
+                # print("original", original.val)
+                # print("visited[original]", visited[original].val)
                 return visited[original]
             
             clone = Node(original.val)
+            # print("clone", clone.val)
             visited[original] = clone
-            
+            # print("visited[original]", (visited[original]).val)
+
+            # print("original.neighbors", original.neighbors)
             for neighbor in original.neighbors:
+                # print("neighbor", neighbor.val)
+                # print("CALLING ######################################")
                 clone.neighbors.append(dfs(neighbor))
+                # print("clone.neighbors.val", clone.neighbors)
             
             return clone
-        
-        return dfs(node)        
+        return dfs(node)   
