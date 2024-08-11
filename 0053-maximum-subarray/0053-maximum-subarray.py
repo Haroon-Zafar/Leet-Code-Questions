5327757ab8687225fbc65f186ff85b1ad5263cb9
Muhammad Haroon Zafar <62334575+Haroon-Zafar@users.noreply.github.com>
2024-08-11 23:42:17 +0500

    Time: 498 ms (85.53%), Space: 31.2 MB (12.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,36 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # using kadane' algorithm
-        # current_sum = nums[0]
-        # maximum_sum = nums[0]
-
-        # # If there is only a single element in the list
-        # if len(nums) == 1 :
-        #     return(nums[0])
-        
-        # for index in range(1, len(nums)):
-
-        #     # if sum is negative
-        #     if current_sum < 0 :
-        #         current_sum = nums[index]
-            
-        #     # if sum is positive 
-        #     else : 
-        #         current_sum += nums[index]
-            
-        #     if current_sum > maximum_sum : 
-        #         # update the maximum_sum 
-        #         maximum_sum = current_sum
-        # return (maximum_sum)
-
-        max_current = max_global = nums[0]  # Initialize both variables with the first element of the array
-
+        max_current = nums[0]
+        max_global = nums[0]
         for num in nums[1:]:
-            # Update max_current to be the maximum of current element and the sum so far
-            max_current = max(num, max_current + num)
+            if max_current + num > num :
+                max_current = max_current + num
+            else:
+                max_current = num
             
-            # Update max_global if max_current is greater
-            max_global = max(max_global, max_current)
-
-        return max_global
-            
+            if max_current > max_global :
+                max_global = max_current
+        return (max_global)
